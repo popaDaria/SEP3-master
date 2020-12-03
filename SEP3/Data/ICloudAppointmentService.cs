2ef717e850af0e54e5727d3b4a28cd43a53766a4
Bogdan Mezei <bogdan335@hotmail.com>
2020-12-03 15:55:37 +0100

    schedule wip

--- SEP3/Data/ICloudAppointmentService.cs
+++ SEP3/Data/ICloudAppointmentService.cs
@@ -1,7 +1,12 @@
-﻿namespace SEP3.Data
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using SEP3.Model;
+
+namespace SEP3.Data
 {
     public interface ICloudAppointmentService
     {
-        
+        public Task<List<Appointment>> GetAllForPatient(int id);
+        public Task<List<Appointment>> GetAllForDoctor(int id);
     }
 }
---
+++ SEP3/Model/Appointment.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SEP3.Model
+{
+    public class Appointment
+        {
+            public string Subject { get; set; }
+            public DateTime StartTime { get; set; }
+            public DateTime EndTime { get; set; }
+            public int patientID { get; set; }
+            public int doctorID { get; set; }
+        }
+}

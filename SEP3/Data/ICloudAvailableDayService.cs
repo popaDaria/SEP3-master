9e588eb224e35b241f22e2da9a2ff1c95be47d24
Daria Popa <popadaria19@gmail.com>
2020-12-07 12:07:37 +0100

    updated some logic for adding appointments

---
+++ SEP3/Data/ICloudAvailableDayService.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using SEP3.Model;
+
+namespace SEP3.Data
+{
+    public interface ICloudAvailableDayService
+    {
+        public Task<List<AvailableDay>> GetAvailableDays(int doctorId);
+        public Task AddAvailableDay(AvailableDay availableDay);
+        public Task UpdateAvailableDay(AvailableDay availableDay);
+    }
+}

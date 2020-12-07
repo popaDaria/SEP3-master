c1f954f14a0856720c90dd283e334c7930dedf19
Daria Popa <popadaria19@gmail.com>
2020-12-07 18:47:40 +0100

    changed appointments

--- SEP3/Model/Appointment.cs
+++ SEP3/Model/Appointment.cs
@@ -6,8 +6,8 @@ namespace SEP3.Model
     {
         public int patientId { get; set; }
         public int doctorId { get; set; }
-        public DateTime appointmentTime { get; set; }
-        public DateTime appointmentDate { get; set; }
+        public DateTime startTime { get; set; }
+        public DateTime endTime { get; set; }
         public string summary { get; set; }
     }
 }
